43a3c7ed8a32cdccfbaea88cb3bdac59140c3caa
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-17 09:49:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Roxanne."
+  end
+
+  get '/hometown' do
+    "My hometown is Queens, NYC."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Vile Mentality by J. Cole."
+  end
 end
